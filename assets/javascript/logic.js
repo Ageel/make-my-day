5719290611f6aff44b96b1aad16c7db3c5e62f35
TREX Hughes <TREX@Traviss-MacBook-Pro-2.local>
2017-04-22 14:02:11 -0500

    This is the most current version of the UI as of the end fo class Saturday

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,30 +1,34 @@
 $("#button1").on('click', function(){
 	event.preventDefault();
-	console.log('Click, damn it');
 	$("#page1").css('display', 'none');
-	console.log('Click, damn it')
 	$("#page2").css('display', 'block');
-	console.log('Click, damn it')
 	$("#button1").css('display', 'none');
-	console.log('Click, damn it')
 	$("#button2").css('display', 'block');
-	console.log('Click, damn it')
 });
 
 $("#button2").on('click', function(){
 	event.preventDefault();
-	console.log('Click, damn it');
 	$("#page2").css('display', 'none');
-	console.log('Click, damn it')
 	$("#page3").css('display', 'block');
-	console.log('Click, damn it')
 	$("#button2").css('display', 'none');
-	console.log('Click, damn it')
-	$('#main-content').css('height', '550px')
-	$('#main-content').css('background-image', 'url("../images/nightlife2.jpg")');
+	$("#button3").css('display', 'block');
 });
+
+$("#button2").on('click', function(){
+	event.preventDefault();
+	$("#page2").css('display', 'none');
+	$("#page3").css('display', 'block');
+	$("#button2").css('display', 'none');
+});
+
 
 $( function() {
     $( "#datepicker" ).datepicker();
     $('#ui-datepicker-div').css('background-color', 'white');
   } );
+
+$('document').on('click', 'li', function(){
+	var choice = $(this).val();
+	$('button').text(choice);
+
+});
