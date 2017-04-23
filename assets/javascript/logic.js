2c3245bb3118bbd0d3868df4fc76b9497e29fec3
TREX Hughes <TREX@Traviss-MacBook-Pro-2.local>
2017-04-22 21:36:01 -0500

    Added locations for page 3, 4, 5, and 6. Updated dropdown to fill on selection. Beginning on the event screen then will sart final screen

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -11,48 +11,47 @@ $("#button2").on('click', function(){
 	$("#page2").css('display', 'none');
 	$("#page3").css('display', 'block');
 	$("#button2").css('display', 'none');
+});
+
+$("#restaurant").on('click', function(){
+	event.preventDefault();
+	$("#page3").css('display', 'none');
+	$("#page4").css('display', 'block');
 	$("#button3").css('display', 'block');
 });
 
 $("#button3").on('click', function(){
 	event.preventDefault();
-	$("#page3").css('display', 'none');
-	$("#page4").css('display', 'block');
+	$("#page4").css('display', 'none');
+	$("#page5").css('display', 'block');
 	$("#button3").css('display', 'none');
 	$("#button4").css('display', 'block');
 });
 
 $("#button4").on('click', function(){
 	event.preventDefault();
-	$("#page4").css('display', 'none');
-	$("#page5").css('display', 'block');
+	$("#page5").css('display', 'none');
+	$("#page6").css('display', 'block');
 	$("#button4").css('display', 'none');
 	$("#button5").css('display', 'block');
 });
 
 $("#button5").on('click', function(){
 	event.preventDefault();
-	$("#page5").css('display', 'none');
-	$("#page6").css('display', 'block');
-	$("#button5").css('display', 'none');
-	$("#button6").css('display', 'block');
-});
-
-$("#button6").on('click', function(){
-	event.preventDefault();
 	$("#page6").css('display', 'none');
 	$("#page1").css('display', 'block');
-	$("#button6").css('display', 'none');
+	$("#button5").css('display', 'none');
 	$("#button1").css('display', 'block');
 });
 
 
 $( function() {
-    $( "#datepicker" ).datepicker();
+    $("#datepicker").datepicker();
+    $('#datepicker').on('click', function() {
     $('#ui-datepicker-div').css('background-color', 'white');
+});
   } );
 
 $(document).on('click', '.dropdown-menu li a', function() {
     $('#event-type').val($(this).html());
 });
-
